a4fb87628790fe897129383620196c1f3b0ded40
dig <70334760+diganta121@users.noreply.github.com>
2025-02-08 16:36:51 +0530

    switched to espnow, mpu error

--- PlaneV1/PlaneV1.ino
+++ PlaneV1/PlaneV1.ino
@@ -1,25 +1,15 @@
-// code for plane esp32 c3
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-#include <Wire.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <esp_now.h>
 #include <MPU9250_WE.h>
+#include <Wire.h>
 #include <ESP32Servo.h>
-#include <Arduino.h>
-#include <driver/ledc.h> // LEDC functions
+#include <driver/ledc.h>
 
-// NRF24L01 Pins
-#define CE_PIN 2
-#define CSN_PIN 3
-RF24 radio(CE_PIN, CSN_PIN);
-const uint64_t address = 0xFCDBA0690;
-
-// Motor/Servo Pins
 #define MOTOR_PWM 5
 #define SERVO_AIL 8
 #define SERVO_ELEV 10
 
-// PID Gains
 #define KP_ROLL 2.0  
 #define KI_ROLL 0.02 
 #define KD_ROLL 0.5  
@@ -28,44 +18,57 @@ const uint64_t address = 0xFCDBA0690;
 #define KI_PITCH 0.03 
 #define KD_PITCH 0.6  
 
-// Complementary Filter
 #define FILTER_ALPHA 0.98  
 #define SIGNAL_TIMEOUT 1000
 
-// LEDC Configuration
 #define LEDC_MODE       LEDC_LOW_SPEED_MODE
 #define LEDC_TIMER      LEDC_TIMER_0
 #define LEDC_CHANNEL    LEDC_CHANNEL_0
 #define LEDC_RESOLUTION LEDC_TIMER_12_BIT
 #define LEDC_FREQ       1000
 
-MPU9250_WE mpu = MPU9250_WE(0x68);
+uint8_t newMACAddress[] = { 0x00, 0x1A, 0x2B, 0x3C, 0x4D, 0x5E };
+#define MPU9250_ADDR 0x68
+MPU9250_WE MPU9250 = MPU9250_WE(MPU9250_ADDR);
 Servo aileron, elevator;
 
+#pragma pack(push, 1)
 struct Packet {
   int16_t throttle;
-  int16_t yaw;
-  int16_t pitch;
   int16_t roll;
+  int16_t pitch;
   bool armed;
   bool selfLevel;
 };
+#pragma pack(pop)
 
-Packet rxData;
+Packet rxData = {0 ,0 ,0, false, true};
 unsigned long lastRecv = 0;
 bool prevArmed = false;
 
-// PID Variables
 float rollError, pitchError;
 float prevRollError, prevPitchError;
 float integralRoll, integralPitch;
 unsigned long lastPIDTime = 0;
 
-// Angle Estimation
 float rollAngle = 0, pitchAngle = 0;
 
+void OnDataRecv(const esp_now_recv_info_t *recv_info, const uint8_t *incomingData, int len) {
+  if (len == sizeof(Packet)) {
+    memcpy(&rxData, incomingData, sizeof(rxData));
+    lastRecv = millis();
+    Serial.println("Received data: " + String(rxData.roll) + ", " + String(rxData.pitch) + ", " + String(rxData.throttle));
+    
+    if (!prevArmed && rxData.armed) {
+      Serial.println("ARMED!");
+      integralRoll = 0;
+      integralPitch = 0;
+    }
+    prevArmed = rxData.armed;
+  }
+}
+
 void setupLEDC() {
-  // Configure LEDC timer
   ledc_timer_config_t timer_conf = {
     .speed_mode = LEDC_MODE,
     .duty_resolution = LEDC_RESOLUTION,
@@ -75,7 +78,6 @@ void setupLEDC() {
   };
   ledc_timer_config(&timer_conf);
 
-  // Configure LEDC channel
   ledc_channel_config_t channel_conf = {
     .gpio_num = MOTOR_PWM,
     .speed_mode = LEDC_MODE,
@@ -88,42 +90,37 @@ void setupLEDC() {
 }
 
 void setMotor(bool armed, int16_t throttle) {
-  int16_t power = armed ? constrain(throttle, 0, 4095) : 0;
+  int16_t power = armed ? constrain(throttle, 0, 255) : 0;
   ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, power);
   ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+  Serial.println("Motor: " + String(power));
 }
 
 void setup() {
   Serial.begin(115200);
-  
-  // Initialize NRF24
-  radio.begin();
-  radio.setPALevel(RF24_PA_MAX);
-  radio.openReadingPipe(0, address);
-  radio.startListening();
-
-  // Configure motor PWM
-  setupLEDC();
-
-  // Attach servos
-  aileron.attach(SERVO_AIL);
-  elevator.attach(SERVO_ELEV);
-
-  // Initialize MPU9250
-  Wire.begin(6, 7); // SDA=GPIO6, SCL=GPIO7
-  if(!mpu.init()){
-    Serial.println("MPU9250 not found!");
+  Wire.begin();
+   if(!MPU9250.init()){
+    Serial.println("MPU9250 does not respond");
+  }
+  else{
+    Serial.println("MPU9250 is connected");
+  }
+  WiFi.mode(WIFI_STA);
+  if (esp_wifi_set_mac(WIFI_IF_STA, newMACAddress) != ESP_OK) {
+    Serial.println("Failed to set MAC address");
+  } else {
+    Serial.println("MAC address changed successfully");
+  }
+  delay(100);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
     while(1);
   }
-  mpu.setSampleRateDivider(5);
-
-  mpu.setAccRange(MPU9250_ACC_RANGE_2G);
-  mpu.enableAccDLPF(true);
-  mpu.setAccDLPF(MPU9250_DLPF_1);
-
-  mpu.setGyrRange(MPU9250_GYRO_RANGE_500);
-  mpu.enableGyrDLPF();
-  mpu.setGyrDLPF(MPU9250_DLPF_1);
+  esp_now_register_recv_cb(OnDataRecv);
+  setupLEDC();
+  aileron.attach(SERVO_AIL);
+  elevator.attach(SERVO_ELEV);
+ 
   lastPIDTime = millis();
 }
 
@@ -131,95 +128,52 @@ void updateAngles() {
   unsigned long currentTime = millis();
   float deltaTime = (currentTime - lastPIDTime) / 1000.0;
   lastPIDTime = currentTime;
-
-  // in g
-  xyzFloat acc = mpu.getGValues();
-  xyzFloat gyr = mpu.getGyrValues();
-
-  // Accelerometer angles
+  xyzFloat acc = MPU9250.getGValues();
+  xyzFloat gyr = MPU9250.getGyrValues();
   float accelRoll = atan2(acc.y, acc.z) * 180.0 / PI;
   float accelPitch = atan2(-acc.x, acc.z) * 180.0 / PI;
-
-  // Gyro rates (already in deg/s)
   float gyroRollRate = gyr.x;
   float gyroPitchRate = gyr.y;
-
-  // Complementary filter
-  rollAngle = FILTER_ALPHA * (rollAngle + gyroRollRate * deltaTime) + 
-             (1 - FILTER_ALPHA) * accelRoll;
-  pitchAngle = FILTER_ALPHA * (pitchAngle + gyroPitchRate * deltaTime) + 
-              (1 - FILTER_ALPHA) * accelPitch;
+  rollAngle = FILTER_ALPHA * (rollAngle + gyroRollRate * deltaTime) + (1 - FILTER_ALPHA) * accelRoll;
+  pitchAngle = FILTER_ALPHA * (pitchAngle + gyroPitchRate * deltaTime) + (1 - FILTER_ALPHA) * accelPitch;
+  Serial.println("Angles: " + String(rollAngle) + ", " + String(pitchAngle));
 }
-
-// Rest of the code remains the same as previous version...
-// [Keep the selfLevelControl() and loop() functions unchanged]
 
 void selfLevelControl() {
   updateAngles();
-
-  // Convert transmitter inputs to target angles
-  float targetRoll = map(rxData.roll, -2048, 2048, -30, 30);
-  float targetPitch = map(rxData.pitch, -2048, 2048, -30, 30);
-
-  // PID calculations
+  float targetRoll = map(rxData.roll, -255, 255, -30, 30);
+  float targetPitch = map(rxData.pitch, -255, 255, -30, 30);
   float deltaTime = (millis() - lastPIDTime) / 1000.0;
   lastPIDTime = millis();
-
   rollError = targetRoll - rollAngle;
   pitchError = targetPitch - pitchAngle;
-
   integralRoll += rollError * deltaTime;
   integralPitch += pitchError * deltaTime;
-
   float derivativeRoll = (rollError - prevRollError) / deltaTime;
   float derivativePitch = (pitchError - prevPitchError) / deltaTime;
-
-  float rollCorrection = KP_ROLL * rollError + 
-                        KI_ROLL * integralRoll + 
-                        KD_ROLL * derivativeRoll;
-  
-  float pitchCorrection = KP_PITCH * pitchError + 
-                         KI_PITCH * integralPitch + 
-                         KD_PITCH * derivativePitch;
-
+  float rollCorrection = KP_ROLL * rollError + KI_ROLL * integralRoll + KD_ROLL * derivativeRoll;
+  float pitchCorrection = KP_PITCH * pitchError + KI_PITCH * integralPitch + KD_PITCH * derivativePitch;
   prevRollError = rollError;
   prevPitchError = pitchError;
-
-  // Apply corrections
   aileron.write(constrain(90 + rollCorrection, 0, 180));
   elevator.write(constrain(90 + pitchCorrection, 0, 180));
+  Serial.println("PID: " + String(rollCorrection) + ", " + String(pitchCorrection));
 }
 
 void loop() {
-  if(radio.available()){
-    radio.read(&rxData, sizeof(rxData));
-    lastRecv = millis();
-    
-    if(!prevArmed && rxData.armed){
-      Serial.println("ARMED!");
-      // Reset PID integrals when arming
-      integralRoll = 0;
-      integralPitch = 0;
-    }
-    prevArmed = rxData.armed;
-  }
-
-  // Failsafe
-  if(millis() - lastRecv > SIGNAL_TIMEOUT){
+  if (millis() - lastRecv > SIGNAL_TIMEOUT) {
     rxData.armed = false;
     setMotor(false, 0);
   }
-
-  if(rxData.armed){
+  if (rxData.armed) {
     setMotor(true, rxData.throttle);
+  } else {
+    setMotor(false, 0);
   }
-  if(rxData.selfLevel){
+  if (rxData.selfLevel) {
     selfLevelControl();
+  } else {
+    aileron.write(map(rxData.roll, -255, 255, 0, 180));
+    elevator.write(map(rxData.pitch, -255, 255, 0, 180));
   }
-  else{
-    // Manual overwrite
-    aileron.write(map(rxData.roll, -2048, 2048, 0, 180));
-    elevator.write(map(rxData.pitch, -2048, 2048, 0, 180));
-  }
-  
 }
